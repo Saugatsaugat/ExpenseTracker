890c3f0811dc5a02a88d928c34fb70a0c7e75460
Saugat Thapa <saugatthapa2073@gmail.com>
2025-12-14 21:26:08 -0600

    added constant value for file location

---
+++ constants/ExpenseTrackerConst.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class ExpenseTrackerConst {
+
+    public static final String FILE_LOCATION = "//home//saugat//Desktop//JAVA//ExpenseTracker//expensetracker.csv";
+    
+}
