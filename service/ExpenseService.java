105d18752c8fdd19777875eab920b5edc996b659
Saugat Thapa <saugatthapa2073@gmail.com>
2025-12-15 08:07:29 -0600

    Feature | added necessary service methods inside expense service class.

--- service/ExpenseService.java
+++ service/ExpenseService.java
@@ -1,12 +1,18 @@
 package service;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import constants.ExpenseTrackerConst;
 import model.Expense;
 import wrapper.ExpenseCategoryTypeAndTotal;
-import wrapper.ExpenseMonthAndTotal;
 
 public class ExpenseService {
 
@@ -17,24 +23,49 @@ public class ExpenseService {
      * Expenses.
      */
     public List<Expense> readFromFiletoModel() {
-        // TODO
+        List<Expense> expenses = new ArrayList<>();
 
-        return null;
+        try (BufferedReader br = new BufferedReader(new FileReader(fileLocation))) {
+            String line;
+
+            // Header Escape
+            line = br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                Expense expense = new Expense(values[0], Float.parseFloat(values[1]), LocalDate.parse(values[2]),
+                        values[3]);
+                expenses.add(expense);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return expenses;
     }
 
     /*
      * This method will write the content to csv file.
      */
-    public void writeToFile() {
-
+    public void writeToFile(Expense expense) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileLocation, true))) {
+            String data = expense.getCategory().toLowerCase() + "," + String.valueOf(expense.getAmount()) + ","
+                    + expense.getDate().toString() + "," + expense.getNote();
+            bw.newLine();
+            bw.write(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /*
      * This method will add new expense
      */
     public boolean createExpense(Expense expense) {
-        // TODO
-
+        try{
+            writeToFile(expense);
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         return false;
     }
 
@@ -42,18 +73,26 @@ public class ExpenseService {
      * This method will return list of all expenses
      */
     public List<Expense> getAllExpense() {
-        // TODO
-
-        return null;
+        return readFromFiletoModel();
     }
 
     /*
      * This method will return the list of all expenses filtered by the date range.
      */
     public List<Expense> filterByDateRange(LocalDate fromDate, LocalDate toDate) {
-        // TODO
+        List<Expense> expenses = readFromFiletoModel();
+        List<Expense> filteredExpenses = new ArrayList<>();
 
-        return null;
+        if(!expenses.isEmpty()){
+            for(Expense expense: expenses){
+                LocalDate expenseDate = LocalDate.of(expense.getDate().getYear(), expense.getDate().getMonthValue(), expense.getDate().getDayOfMonth());
+                // System.out.println(expenseDate+ " f: "+ fromDate + " t: "+ toDate);
+                if(expenseDate.isAfter(fromDate) && expenseDate.isBefore(toDate)){
+                    filteredExpenses.add(expense);
+                }
+            }
+        }
+        return filteredExpenses;
     }
 
     /*
@@ -61,9 +100,17 @@ public class ExpenseService {
      * type.
      */
     public List<Expense> filterByCategory(String category) {
-        // TODO
+        List<Expense> expenses = readFromFiletoModel();
+        List<Expense> filteredExpenses = new ArrayList<>();
 
-        return null;
+        if(!expenses.isEmpty()){
+            for(Expense expense: expenses){
+                if(expense.getCategory().equals(category)){
+                    filteredExpenses.add(expense);
+                }
+            }
+        }
+        return filteredExpenses;
     }
 
     /*
@@ -71,19 +118,66 @@ public class ExpenseService {
      * wrapper class.
      */
     public List<ExpenseCategoryTypeAndTotal> getTotalByCategory() {
-        // TODO
-
-        return null;
+        List<Expense> expenses = readFromFiletoModel();
+        var dataList = getCategoryAndExpenseList(expenses);
+        return dataList;
     }
 
     /*
      * This method will return the list of month and their total sum in a wrapper
      * class.
      */
-    public List<ExpenseMonthAndTotal> getTotalByMonth() {
-        // TODO
+    public List<ExpenseCategoryTypeAndTotal> getTotalByMonth(int month, int year) {
+        List<Expense> expenses = readFromFiletoModel();
+        List<Expense> filteredDataList = new ArrayList<>();
 
-        return null;
+        for(Expense expense: expenses){
+            if((expense.getDate().getMonthValue() == month) && (expense.getDate().getYear() == year)){
+                filteredDataList.add(expense);
+            }
+        }
+        var dataList = getCategoryAndExpenseList(filteredDataList);
+
+       return dataList;
     }
+
+    /*
+    This method will return list of unique expense categories
+    */
+   public Set<String> getUniqueExpenseCategory(){
+    List<Expense> expenses = readFromFiletoModel();
+    Set<String> uniqueCategories = new HashSet<>();
+    if(!expenses.isEmpty()){
+        for(Expense expense:expenses){
+            uniqueCategories.add(expense.getCategory());
+        }
+    }
+    return uniqueCategories;
+   }
+
+   /*
+   This method will return list of expense category type and it's total sum of amount.
+   */
+  public List<ExpenseCategoryTypeAndTotal> getCategoryAndExpenseList(List<Expense> expenses){
+    List<ExpenseCategoryTypeAndTotal> dataList = new ArrayList<>();
+    Set<String> uniqueExpenseCategories = getUniqueExpenseCategory();
+        
+        for(String category:uniqueExpenseCategories){
+
+            ExpenseCategoryTypeAndTotal expenseCategoryTypeAndTotal = new ExpenseCategoryTypeAndTotal();
+            expenseCategoryTypeAndTotal.setCategory(category);
+            float sum = 0.0f;
+
+            for(Expense expense: expenses){
+                if(expense.getCategory().equals(category)){
+                    sum += expense.getAmount();
+                }
+            }
+            expenseCategoryTypeAndTotal.setTotal(sum);
+
+            dataList.add(expenseCategoryTypeAndTotal);
+    }
+    return dataList;
+  }
 
 }
