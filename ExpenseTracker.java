b288c8cb796dd56ca973aac7cce6d6af3425cf6e
Saugat Thapa <saugatthapa2073@gmail.com>
2025-12-14 20:58:04 -0600

    Solved issue of file creation failed

--- ExpenseTracker.java
+++ ExpenseTracker.java
@@ -1,11 +1,8 @@
 import util.Configuration;
 
 public class ExpenseTracker{
-    public ExpenseTracker(){
-        Configuration conf = new Configuration();
-    }
-    
+
     public static void main(String[] args) {
-        
+        Configuration conf = new Configuration();
     }
 }
